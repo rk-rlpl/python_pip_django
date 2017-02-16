d7796f3dfe7dd2d71f0da3b33ab42132911ec6be
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-16 15:47:14 +0530

    initial commit added failing test for django version

---
+++ README.md
@@ -0,0 +1,4 @@
+# python_pip_django
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: python_pip_django
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
