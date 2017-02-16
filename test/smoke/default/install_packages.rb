a4bb7ced437a89cab012ee7b45dc6a68d40c95c3
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-16 19:56:22 +0530

    implements installation by package
    Install django with package

--- test/smoke/default/install_packages.rb
+++ test/smoke/default/install_packages.rb
@@ -1,11 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe python_pip_django::default
+# Inspec test for recipe python_pip_django::install_packages.rb
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
-
-
-describe command('django-admin --version') do
-  its(:stdout) { should match(/1.8/)}
-end
