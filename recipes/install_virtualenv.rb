8b42f4f130fb8add6bcd179593edf760d9072145
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-20 11:02:02 +0530

    Install django from virtualenv
    Added new integration test suite

---
+++ recipes/install_virtualenv.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: python_pip_django
+# Recipe:: install_virtualenv
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
---
+++ test/smoke/install_virtualenv/default_test.rb
@@ -0,0 +1,11 @@
+# # encoding: utf-8
+
+# Inspec test for recipe python_pip_django::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+
+describe command('django-admin --version') do
+  its(:stdout) { should match(/1.6.1/)}
+end
